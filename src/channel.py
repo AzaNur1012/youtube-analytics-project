982d517a3a603000fdf733f7c2909b24be4cddfe
Nuriev Azat <aznuri@yandex.ru>
2024-01-11 00:01:08 +0500

    hw 1-2

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,26 @@
+import json
+import os
+from googleapiclient.discovery import build
 
+#class YoutubeParser:
+    #api_key = os.getenv('YT_API_KEY')
+    #youtube = build('youtube', 'v3', developerKey=api_key)
+
+    #@classmethod
+    #def get_channel(cls,channel_id):
+        #yt_channel_all_info = cls.youtube.channels().list(id=channel_id, part='snippet,statistics').execute
+        #return yt_channel_all_info
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube: object = build('youtube', 'v3', developerKey=api_key)
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        #self.info = YoutubeParser.get_channel(channel_id)
 
-    def print_info(self) -> None:
+    def print_info(self):
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.channel_id))
