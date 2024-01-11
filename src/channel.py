bb43153e2a350b2b42a1be0f77f267fd4888a65e
Nuriev Azat <aznuri@yandex.ru>
2024-01-11 23:44:30 +0500

    hw 1-2 исправления последние

--- src/channel.py
+++ src/channel.py
@@ -11,6 +11,7 @@ class Channel:
         self.channel_id = channel_id
         self.youtube = build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
         self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel,indent=2, ensure_ascii=False))
