feaf2f0d8f4dba9173a2ebe0bacd41e7564fb9f7
Nuriev Azat <aznuri@yandex.ru>
2024-01-11 23:41:04 +0500

    hw 1-2 исправления

--- src/channel.py
+++ src/channel.py
@@ -1,26 +1,19 @@
 import json
 import os
+import isodate
 from googleapiclient.discovery import build
-
-#class YoutubeParser:
-    #api_key = os.getenv('YT_API_KEY')
-    #youtube = build('youtube', 'v3', developerKey=api_key)
-
-    #@classmethod
-    #def get_channel(cls,channel_id):
-        #yt_channel_all_info = cls.youtube.channels().list(id=channel_id, part='snippet,statistics').execute
-        #return yt_channel_all_info
 
 class Channel:
     """Класс для ютуб-канала"""
 
     api_key: str = os.getenv('YT_API_KEY')
-    youtube: object = build('youtube', 'v3', developerKey=api_key)
+    youtube: str = build('youtube', 'v3', developerKey=api_key)
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        #self.info = YoutubeParser.get_channel(channel_id)
-
-    def print_info(self):
+        self.youtube = build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
+        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+    def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(json.dumps(self.channel_id))
+        print(json.dumps(self.channel,indent=2, ensure_ascii=False))
